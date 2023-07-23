e2c3a8463bbfb3eded50259cd8da767fea0d0a6d
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2023-07-23 20:36:30 +0530

    Add files via upload

---
+++ update.js
@@ -0,0 +1,77 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// form submit event
+form.addEventListener('submit', addItem);
+
+itemList .addEventListener('click' , removeItem);
+
+filter.addEventListener('keyup' , filterItems);
+
+// function for addItem
+function addItem(e){
+    e.preventDefault();
+    
+    //Get input value
+    var newItem = document.getElementById('item').value;
+
+    // create new li element
+
+    var li = document.createElement('li');
+
+    //Add Class
+
+    li.className = 'list-group-item';
+    
+    //Add textNode with input value
+
+    li.appendChild(document.createTextNode(newItem));
+
+    // create delete button element
+
+    var deleteBtn = document.createElement('button');
+
+    //Add class to delete button
+
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+    //Add textNode 
+
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Appned button to li
+    li.appendChild(deleteBtn);
+
+    // Append li to List 
+    itemList.appendChild(li);
+}
+
+//REMOVE ITEM
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+
